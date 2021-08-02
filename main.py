808e997eabc504526d7280635d9ec62e50d92d9f
Avery Chan <JustinAveryChan@gmail.com>
2021-08-01 19:53:47 -0500

    add first test graph

--- main.py
+++ main.py
@@ -32,27 +32,35 @@ print(len(samples))
 # sample_rate = len(samples)
 # print(samples.tolist())
 
-# rng = np.random.default_rng()
 
+
+# !
+rng = np.random.default_rng()
 # # Generate a test signal, a 2 Vrms sine wave whose frequency is slowly modulated around 3kHz, corrupted by white noise of exponentially decreasing magnitude sampled at 10 kHz.
-# fs = 10e3
-# N = 1e5
-# amp = 2 * np.sqrt(2)
-# noise_power = 0.01 * fs / 2
-# time = np.arange(N) / float(fs)
-# mod = 500*np.cos(2*np.pi*0.25*time)
-# carrier = amp * np.sin(2*np.pi*3e3*time + mod)
-# noise = rng.normal(scale=np.sqrt(noise_power), size=time.shape)
-# noise *= np.exp(-time/5)
-# x = carrier + noise
+fs = 10e3
+N = 1e5
+amp = 2 * np.sqrt(2)
+noise_power = 0.01 * fs / 2
+time = np.arange(N) / float(fs)
+mod = 500*np.cos(2*np.pi*0.25*time)
+carrier = amp * np.sin(2*np.pi*3e3*time + mod)
+noise = rng.normal(scale=np.sqrt(noise_power), size=time.shape)
+noise *= np.exp(-time/5)
+x = carrier + noise
+
+# Compute and plot the spectrogram.
+f, t, Sxx = signal.spectrogram(x, fs)
+
+plt.pcolormesh(t, f, Sxx, shading='gouraud')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.show()
+# !
 
 Pxx, freqs, bins, im = plt.specgram(samples, NFFT=1024, Fs=44100, noverlap=900)
 # plt.yscale('log')
 # plt.xscale('log')
 plt.show()
-
-# # Compute and plot the spectrogram.
-# f, t, Sxx = signal.spectrogram(x, fs)
 
 print("making spectrogram...")
 f, t, Sxx = signal.spectrogram(samples, sample_rate, mode='magnitude')
@@ -66,8 +74,8 @@ print("making graph...")
 # plt.xlabel('Time [sec]')
 # plt.show()
 
-# plt.pcolormesh(t, f, np.log10(Sxx)*100, shading='gouraud')
-plt.pcolormesh(t, f, Sxx, shading='gouraud')
+plt.pcolormesh(t, f, np.log10(Sxx), shading='gouraud')
+# plt.pcolormesh(t, f, Sxx, shading='gouraud')
 plt.imshow(Sxx)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
